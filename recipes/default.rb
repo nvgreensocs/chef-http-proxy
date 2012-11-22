e4c6900a24de17350387b3ac598da309115856f4
Mark Burton <mark@greensocs.com>
2012-11-22 15:19:12 +0100

    Script to add http_proxy

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,1 +1,14 @@
-package ""
+
+
+bash "Set HTTP_PROXY " do
+  cwd Chef::Config[:file_cache_path]
+  code <<-EOH
+
+  cd /vagrant/
+  cp .http_proxy /etc/bash.bashrc.http_proxy
+  echo "source /etc/bash.bashrc.http_proxy" >> /etc/bash.bashrc
+  EOH
+  creates "/etc/bash.bashrc.http_proxy"
+end
+
+
