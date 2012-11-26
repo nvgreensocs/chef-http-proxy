d7e4b662118ff0c4cc5754f1a39e0cb7f976989f
Mark Burton <mark@greensocs.com>
2012-11-26 15:03:06 +0100

    More http proxy stuff

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,10 +4,15 @@ bash "Set HTTP_PROXY " do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
 
+
   cd /vagrant/
-  cp .http_proxy /etc/bash.bashrc.http_proxy
+  echo "export http_proxy=\'#{Chef::Config[:http_proxy]}\'" > /etc/bash.bashrc.http_proxy
   echo "source /etc/bash.bashrc.http_proxy" >> /etc/bash.bashrc
   echo 'Defaults env_keep = "http_proxy https_proxy ftp_proxy"' >> /etc/sudoers
+  echo "Acquire::http::Proxy \"#{Chef::Config[:http_proxy]}\";" > /etc/apt/apt.conf.d/30proxy
+
+  
+  environment { 'http_proxy' => Chef::Config[:http_proxy] }
 
 
   EOH
